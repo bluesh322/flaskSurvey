814d30c799cbcac9480c4dc6ad570c204a05f14c
bluesh322 <bluesh322@gmail.com>
2020-03-12 11:50:56 -0500

    added stateful feature, and multiple survey functionality

--- app.py
+++ app.py
@@ -7,29 +7,47 @@ app.config['SECRET_KEY'] = 'agooddaytodie@tacobell'
 debug = DebugToolbarExtension(app)
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 RESPONSES_KEY = "responses"
+CURRENT_SURVEY = "curr_survey"
 
 responses = []
 
 @app.route('/')
-def index():
-    """Show list of stories"""
+def show_choose_survey_form():
+    """Show list of surveys"""
     session[RESPONSES_KEY] = []
     return render_template('index.html',
-    title=satisfaction_survey.title,
-    instructions=satisfaction_survey.instructions,
     surveys=surveys.values())
+
+@app.route('/', methods=["POST"])
+def choose_survey():
+    """Select a Survey"""
+    survey_title = request.form['survey_title']
+
+    survey = surveys[survey_title]
+    session[CURRENT_SURVEY] = survey_title
+
+    return render_template('start-survey.html', survey=survey)
+
+@app.route('/begin', methods=["POST"])
+def begin():
+    """my understanding is that you want to eat the initial POST REQ so show_questions 
+    only recieves redirect GET requests from /answer, this feels bad, unless i do an initialization here"""
+    return redirect('/questions/0')
+
 
 @app.route('/questions/<int:que_num>')
 def show_question(que_num):
     """Show question and show answers if needed"""
     responses = session.get(RESPONSES_KEY)
+    survey_title = session[CURRENT_SURVEY]
+    survey = surveys[survey_title]
 
-    choices = satisfaction_survey.questions[que_num].choices
-    question = satisfaction_survey.questions[que_num]
+    choices = survey.questions[que_num].choices
+    question = survey.questions[que_num]
 
     if (responses is None):
         return redirect("/")
-    if (len(responses) == len(satisfaction_survey.questions)):
+    if (len(responses) == len(survey.questions)):
         # They've answered all the questions! Thank them.
         return redirect("thankyou.html")
     if que_num != len(responses):
@@ -45,12 +63,38 @@ def show_question(que_num):
 def answer():
     """Add answer to responses and push back to question, add 1 to que_num"""
     responses = session[RESPONSES_KEY]
+    survey_title = session[CURRENT_SURVEY]
+    survey = surveys[survey_title]
     responses.append(request.form['answer'])
     session[RESPONSES_KEY] = responses
+
     que_num = request.form['que_num']
     que_num = int(que_num)+1
     
-    if (len(responses) == len(satisfaction_survey.questions)):
+    if (len(responses) == len(survey.questions)):
         return render_template('thankyou.html')
     else: 
         return redirect(f'/questions/{str(que_num)}')
+
+# ***********************************OTHER CODE FOR LEARNING SESSIONS STUFF ******************************************
+
+@app.route('/party')
+def show_invite():
+    if session.get("entered-pin", False):
+        return render_template('secret.html')
+    else:
+        return redirect('/login-form')
+
+@app.route('/login-form')
+def log_in_form():
+    return render_template('log-in.html')
+
+@app.route('/login')
+def login():
+    SECRET = 'ilikedogs'
+    code = request.args['secret_code']
+    if code == SECRET:
+        session["entered-pin"] = True
+        return redirect("/party")
+    else:
+        return redirect("/login-form")
